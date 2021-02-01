12945a77dd6bb1d658f432ac71a581162226d866
Elliot Dodd <elliot.dodd94@gmail.com>
2021-02-01 19:44:17 +0000

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -25,6 +25,33 @@ else {
 					<a href="index.php" class="logo">
 						<img src="assets/images/logo.png">
 					</a>
+
+					<div class="group">
+							<div class="navItem">
+								<a href="search.php" class="navItemLink">Search
+								</a>
+
+							</div>
+					</div>
+
+					<div class="group">
+						<div class="navItem">
+								<a href="browse.php" class="navItemLink">Browse
+								</a>
+
+							</div>
+							<div class="navItem">
+								<a href="libray.php" class="navItemLink">Library
+								</a>
+
+							</div>
+							<div class="navItem">
+								<a href="profile.php" class="navItemLink">Your account
+								</a>
+
+							</div>
+						
+					</div>
 				</nav>
 				
 
